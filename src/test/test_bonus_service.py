f459d674eb3a5bd750c5f5d87fe51ce0c4c44f0f
Emmanuel Carreira Alves <eca2@cin.ufpe.br>
2021-10-31 00:53:23 +0000

    test: adds test case for when employee's salary is exactly 10000

--- src/test/test_bonus_service.py
+++ src/test/test_bonus_service.py
@@ -7,11 +7,16 @@ from src.main.employee import Employee
 
 class BonusServiceTest(unittest.TestCase):
     def test_bonus_is_zero_when_employee_salary_is_greater_than_10000(self):
-        employee = Employee('Test', 11000.0, datetime.utcnow())
+        employee = Employee('Test', 11000, datetime.utcnow())
         bonus = bonus_service.calculate_bonus(employee)
         self.assertEqual(bonus, 0)
 
     def test_bonus_is_one_tenth_of_the_salary_when_employee_salary_is_less_than_10000(self):
-        employee = Employee('Test', 1000.0, datetime.utcnow())
+        employee = Employee('Test', 1000, datetime.utcnow())
         bonus = bonus_service.calculate_bonus(employee)
         self.assertEqual(bonus, 100)
+
+    def test_bonus_is_one_tenth_of_the_salary_when_employee_salary_is_exactly_10000(self):
+        employee = Employee('Test', 10000, datetime.utcnow())
+        bonus = bonus_service.calculate_bonus(employee)
+        self.assertEqual(bonus, 1000)
