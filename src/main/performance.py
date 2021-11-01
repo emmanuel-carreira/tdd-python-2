c19fcc608b2f598aea4d5169f75856605e3c076f
Emmanuel Carreira Alves <eca2@cin.ufpe.br>
2021-11-01 02:26:14 +0000

    refactor: passes readjustment percent obtention to Performance enum

--- src/main/performance.py
+++ src/main/performance.py
@@ -4,3 +4,12 @@ class Performance(Enum):
     BELOW_EXPECTATIONS = 1
     GOOD = 2
     GREAT = 3
+
+    @staticmethod
+    def get_readjustment_percentage(performance: int) -> float:
+        performance_strategy = {
+            Performance.BELOW_EXPECTATIONS: 0.03,
+            Performance.GOOD: 0.15,
+            Performance.GREAT: 0.2
+        }
+        return performance_strategy.get(performance, 0)
