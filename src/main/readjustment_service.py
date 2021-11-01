90fc3c3f34fa0344d87934caafebdff33c452598
Emmanuel Carreira Alves <eca2@cin.ufpe.br>
2021-11-01 02:28:09 +0000

    refactor: transforms ReadjustmentService class into a module

--- src/main/readjustment_service.py
+++ src/main/readjustment_service.py
@@ -2,7 +2,6 @@ from src.main.employee import Employee
 from src.main.performance import Performance
 
 
-class ReadjustmentService:
-    def readjust_salary(self, employee: Employee, performance: Performance) -> None:
-        readjustment = 1 + Performance.get_readjustment_percentage(performance)
-        employee.readjust_salary(readjustment)
+def readjust_salary(employee: Employee, performance: Performance) -> None:
+    readjustment = 1 + Performance.get_readjustment_percentage(performance)
+    employee.readjust_salary(readjustment)
