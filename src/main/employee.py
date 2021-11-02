508734d59a1069464b03eb6aa3264dcf884cd978
Emmanuel Carreira Alves <eca2@cin.ufpe.br>
2021-11-02 01:09:35 +0000

    refactor: turns Employee class into dataclass

--- src/main/employee.py
+++ src/main/employee.py
@@ -1,23 +1,12 @@
+from dataclasses import dataclass
 from datetime import datetime
 
 
+@dataclass
 class Employee:
-    def __init__(self, name: str, salary: float, admission_date: datetime) -> None:
-        self._name = name
-        self._salary = salary
-        self._admission_date = admission_date
-
-    @property
-    def admission_date(self):
-        return self._admission_date
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def salary(self):
-        return self._salary
+    name: str
+    salary: float
+    admission_date: datetime
 
     def readjust_salary(self, readjustment: float) -> None:
-        self._salary *= readjustment
+        self.salary *= readjustment
