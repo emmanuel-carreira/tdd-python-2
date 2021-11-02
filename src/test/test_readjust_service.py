ab5f6d2a06b91194b534cfbf83596688dbfb6d54
Emmanuel Carreira Alves <eca2@cin.ufpe.br>
2021-11-02 00:25:52 +0000

    tests: generalizes employee instanciation

--- src/test/test_readjust_service.py
+++ src/test/test_readjust_service.py
@@ -7,22 +7,21 @@ from src.main.performance import Performance
 
 
 class ReadjustmentServiceTest(unittest.TestCase):
+    def setUp(self):
+        self.employee = Employee('Test', 10000, datetime.utcnow())
+
     def test_salary_readjuste_should_be_three_percent_for_employee_below_expectations(self):
-        employee = Employee('Test', 10000, datetime.utcnow())
-        readjustment_service.readjust_salary(employee, Performance.BELOW_EXPECTATIONS)
-        self.assertEqual(employee.salary, 10300)
+        readjustment_service.readjust_salary(self.employee, Performance.BELOW_EXPECTATIONS)
+        self.assertEqual(self.employee.salary, 10300)
 
     def test_salary_readjuste_should_be_fifteen_percent_for_employee_with_good_performance(self):
-        employee = Employee('Test', 10000, datetime.utcnow())
-        readjustment_service.readjust_salary(employee, Performance.GOOD)
-        self.assertEqual(employee.salary, 11500)
+        readjustment_service.readjust_salary(self.employee, Performance.GOOD)
+        self.assertEqual(self.employee.salary, 11500)
 
     def test_salary_readjuste_should_be_twenty_percent_for_employee_with_great_performance(self):
-        employee = Employee('Test', 10000, datetime.utcnow())
-        readjustment_service.readjust_salary(employee, Performance.GREAT)
-        self.assertEqual(employee.salary, 12000)
+        readjustment_service.readjust_salary(self.employee, Performance.GREAT)
+        self.assertEqual(self.employee.salary, 12000)
 
     def test_salary_is_not_readjusted_when_performance_is_not_valid(self):
-        employee = Employee('Test', 10000, datetime.utcnow())
-        readjustment_service.readjust_salary(employee, 0)
-        self.assertEqual(employee.salary, 10000)
+        readjustment_service.readjust_salary(self.employee, 0)
+        self.assertEqual(self.employee.salary, 10000)
